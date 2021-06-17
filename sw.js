0ea5f41498f58900503368ff5df1b8d6af58743c
3schools <85482920+3schools@users.noreply.github.com>
2021-06-17 12:53:48 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('ask-3schools').then((cache) => cache.addAll([
+    caches.open('3schools-in').then((cache) => cache.addAll([
       'index.html',
       'ask.png',
       'index.js',
